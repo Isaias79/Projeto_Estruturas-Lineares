a5a8c75e17748561b4bad751c46af26f228465d4
Isaias <60967597+Isaias79@users.noreply.github.com>
2024-12-06 17:40:09 +0000

    Exercicio

--- Estruturas Lineares/Exercicio 2 - Balanceamento/script.js
+++ Estruturas Lineares/Exercicio 2 - Balanceamento/script.js
@@ -3,40 +3,27 @@ import {Stack} from "./Stack.js"
 const pushButton = document.querySelector("#balancear_Button")
 
 
-const s = new Stack()
 
 balancear_Button.addEventListener("click", ()=>balancear())
 
 function balancear(){
+    const s = new Stack()
     const texto = document.getElementById("elemento").value
+
+    for (let letra of texto){ 
+        if (letra === "("){
+            s.push(letra)
+        } else if (letra === ")"){
+            if (s.isEmpty()){
+                alert("Não Está Balanceado!")
+                return
+            }
+            s.pop()
+        }
+    }
+    if (s.isEmpty()){
+        alert("Está Balanceado!")
+    }else{
+        alert("Não Está Balanceado!")
+    }
 }
-
-// function pushValue() {
-//   const texto = document.querySelector("#elemento").value
-//   s.push(texto)
-//   alert.log(s.stack)
-//   // console.log(s.stack)
-// }
-
-// function popValue(){
-//   // const texto = document.querySelector("#elemento").value
-//   s.pop()
-//   alert.log(s.stack)
-//   // console.log(s.stack)
-// }
-
-// function peekValue(){
-//   alert.log(s.peek())
-//   // console.log(s.peek())
-// }
-
-// function isemptyValue(){
-  
-//   alert.log(s.isEmpty())
-//   // console.log(s.isEmpty())
-// }
-
-// function sizeValue(){
-//   alert.log(s.size())
-//   // console.log(s.size())
-// }
